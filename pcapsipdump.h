d8473a72c803de6cd4d492ce57586b32ddfc82e2
Dragos Oancea <dragos@nyquist.ro>
2025-03-09 19:12:56 +0200

    Indent with clang-format.

--- pcapsipdump.h
+++ pcapsipdump.h
@@ -28,41 +28,37 @@
 
 struct iphdr {
 #if defined(__LITTLE_ENDIAN)
-	uint8_t	ihl:4,
-		version:4;
-#elif defined (__BIG_ENDIAN)
-	uint8_t	version:4,
-  		ihl:4;
+  uint8_t ihl : 4, version : 4;
+#elif defined(__BIG_ENDIAN)
+  uint8_t version : 4, ihl : 4;
 #else
 #error Endian not defined
 #endif
-	uint8_t	tos;
-	uint16_t	tot_len;
-	uint16_t	id;
-	uint16_t	frag_off;
-	uint8_t	ttl;
-	uint8_t	protocol;
-	uint16_t	check;
-	uint32_t	saddr;
-	uint32_t	daddr;
-	/*The options start here. */
+  uint8_t tos;
+  uint16_t tot_len;
+  uint16_t id;
+  uint16_t frag_off;
+  uint8_t ttl;
+  uint8_t protocol;
+  uint16_t check;
+  uint32_t saddr;
+  uint32_t daddr;
+  /*The options start here. */
 };
 
-
 struct udphdr {
-	uint16_t	source;
-	uint16_t	dest;
-	uint16_t	len;
-	uint16_t	check;
+  uint16_t source;
+  uint16_t dest;
+  uint16_t len;
+  uint16_t check;
 };
 
 #ifndef ETH_ALEN
 #define ETH_ALEN 6
 #endif
 
-struct ether_header
-{
-  uint8_t  ether_dhost[ETH_ALEN];	/* destination eth addr	*/
-  uint8_t  ether_shost[ETH_ALEN];	/* source ether addr	*/
-  uint16_t ether_type;		        /* packet type ID field	*/
-} __attribute__ ((__packed__));
+struct ether_header {
+  uint8_t ether_dhost[ETH_ALEN]; /* destination eth addr	*/
+  uint8_t ether_shost[ETH_ALEN]; /* source ether addr	*/
+  uint16_t ether_type;           /* packet type ID field	*/
+} __attribute__((__packed__));
